8635ca9c489ac2c22005481d487e834a93d758b7
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-30 10:09:37 +0530

    tdd: refactor

--- src/wrdl.ts
+++ src/wrdl.ts
@@ -1,28 +1,37 @@
-export const scoreGuess = (guess: string, answer: string) => {
+export const CORRECT = "C";
+export const ALMOST = "A";
+export const INCORRECT = "I";
+
+export type LetterScore = typeof CORRECT | typeof INCORRECT | typeof ALMOST;
+export type GuessScore = LetterScore[];
+
+const EMPTY = "";
+
+export const scoreGuess = (guess: string, answer: string): GuessScore => {
   const answerLetters = answer.split("");
   const guessLetters = guess.split("");
 
-  const score = [];
+  const score: GuessScore = [];
 
   for (let i = 0; i < guessLetters.length; i++) {
     if (guessLetters[i] === answerLetters[i]) {
-      score[i] = "C";
-      answerLetters[i] = "";
-      guessLetters[i] = "";
+      score[i] = CORRECT;
+      answerLetters[i] = EMPTY;
+      guessLetters[i] = EMPTY;
     }
   }
 
   for (let i = 0; i < guessLetters.length; i++) {
-    if (guessLetters[i] === "") continue;
+    if (guessLetters[i] === EMPTY) continue;
 
-    if (answerLetters.includes(guessLetters[i])) {
-      score[i] = "A";
-      const answerIdx = answerLetters.findIndex(
-        (char) => char === guessLetters[i]
-      );
-      answerLetters[answerIdx] = "";
+    const answerIdx = answerLetters.findIndex(
+      (char) => char === guessLetters[i]
+    );
+    if (answerIdx > -1) {
+      score[i] = ALMOST;
+      answerLetters[answerIdx] = EMPTY;
     } else {
-      score[i] = "I";
+      score[i] = INCORRECT;
     }
   }
 
