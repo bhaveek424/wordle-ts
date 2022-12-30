50836efc349a293e9f7534ba2d2a176f16e6e4c8
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-30 06:14:10 +0530

    tdd: apt logic for words without duplicates

--- src/wrdl.ts
+++ src/wrdl.ts
@@ -1,3 +1,14 @@
 export const scoreGuess = (guess: string, answer: string) => {
-  return guess === answer ? ["C"] : ["I"];
+  const score = [];
+  for (let i = 0; i < guess.length; i++) {
+    if (guess[i] === answer[i]) {
+      score[i] = "C";
+    } else if (answer.includes(guess[i])) {
+      score[i] = "A";
+    } else {
+      score[i] = "I";
+    }
+  }
+
+  return score;
 };
